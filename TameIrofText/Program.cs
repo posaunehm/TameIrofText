1738b2afcbc7e8d5419f70d946b62458044bd788
posaunehm <posaunehm@gmail.com>
2013-11-29 01:08:10 +0900

    implement Get Timeline feature

--- TameIrofText/Program.cs
+++ TameIrofText/Program.cs
@@ -1,9 +1,4 @@
-﻿
-
-using System.Collections.Generic;
-
-using java.io;
-using java.util;
+﻿using System.Collections.Generic;
 using twitter4j;
 
 namespace TameIrofText
@@ -11,79 +6,32 @@ namespace TameIrofText
     internal class Program
     {
         private static void Main(string[] args)
-        {
-            var file = new File("twitter4j.properties");
-            var prop = new Properties();
-            InputStream inputStream = null;
-            OutputStream outputStream = null;
+        {   
             try
             {
-                if (file.exists())
-                {
-                    inputStream = new FileInputStream(file);
-                    prop.load(inputStream);
-                }
-                if (args.Length < 2)
-                {
-                    if (null == prop.getProperty("oauth.consumerKey")
-                        && null == prop.getProperty("oauth.consumerSecret"))
-                    {
-                        // consumer key/secret are not set in twitter4j.properties
-
-                        System.Console.WriteLine(
-                            "Usage: java twitter4j.examples.oauth.GetAccessToken [consumer key] [consumer secret]");
-                        System.Environment.Exit(-1);
-                    }
-                }
-                else
-                {
-                    prop.setProperty("oauth.consumerKey", args[0]);
-                    prop.setProperty("oauth.consumerSecret", args[1]);
-                    outputStream = new FileOutputStream("twitter4j.properties");
-                    prop.store(outputStream, "twitter4j.properties");
-                }
-            }
-            catch (IOException ioe)
-            {
-                ioe.printStackTrace();
-                System.Environment.Exit(-1);
-            }
-            finally
-            {
-                if (inputStream != null)
-                {
-                    try
-                    {
-                        inputStream.close();
-                    }
-                    catch (IOException)
-                    {
-                    }
-                }
-                if (outputStream != null)
-                {
-                    try
-                    {
-                        outputStream.close();
-                    }
-                    catch (IOException)
-                    {
-                    }
-                }
-            }
-            try
-            {
-
                 // gets Twitter instance with default credentials
                 var twitter = new TwitterFactory().getInstance();
                 var user = twitter.verifyCredentials();
-                var statuses = twitter.getHomeTimeline();
-                System.Console.WriteLine("Showing @" + user.getScreenName() + "'s home timeline.");
-                foreach (var status in statuses.ToEnumerable<Status>())
-                {
-                    System.Console.WriteLine("@" + status.getUser().getScreenName() + " - " + status.getText());
-                }
 
+                
+                System.Console.WriteLine("Showing @" + user.getScreenName() + "'s home timeline.");
+                int i = 1;
+                while (true)
+                {
+                    var statuses = twitter.getHomeTimeline(new Paging(i));
+
+                    foreach (var status in statuses.ToEnumerable<Status>())
+                    {
+                        System.Console.WriteLine("@" + status.getUser().getScreenName() + " - " + status.getText());
+                    }
+
+                    if (System.Console.ReadKey().KeyChar == 'q')
+                    {
+                        break;
+                    }
+
+                    i++;
+                }
             }
             catch (TwitterException te)
             {
