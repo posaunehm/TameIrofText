c2565bda383ff7b1341b5eef43f15b298663e638
posaunehm <posaunehm@gmail.com>
2013-11-29 08:05:58 +0900

    tokenize timeline and show only known noun

--- TameIrofText/Program.cs
+++ TameIrofText/Program.cs
@@ -1,31 +1,45 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using java.util;
+using org.atilika.kuromoji;
 using twitter4j;
+
+using CliConsole = System.Console;
 
 namespace TameIrofText
 {
     internal class Program
     {
         private static void Main(string[] args)
-        {   
+        {
             try
             {
+                // Get tokenizer
+                var tokenizer = Tokenizer.builder().build();
+
                 // gets Twitter instance with default credentials
                 var twitter = new TwitterFactory().getInstance();
                 var user = twitter.verifyCredentials();
 
-                
-                System.Console.WriteLine("Showing @" + user.getScreenName() + "'s home timeline.");
-                int i = 1;
+
+                CliConsole.WriteLine("Showing @" + user.getScreenName() + "'s home timeline.");
+                var i = 1;
                 while (true)
                 {
                     var statuses = twitter.getHomeTimeline(new Paging(i));
 
                     foreach (var status in statuses.ToEnumerable<Status>())
                     {
-                        System.Console.WriteLine("@" + status.getUser().getScreenName() + " - " + status.getText());
+                        var text = tokenizer.tokenize(status.getText()).ToEnumerable<Token>().Where(t =>t.getAllFeaturesArray()[0] == "名詞" && t.isKnown());
+
+                        CliConsole.WriteLine("@" + status.getUser().getScreenName() + " - " );
+                        foreach (var token in text)
+                        {
+                            CliConsole.WriteLine("\t" + token.getAllFeatures() + " : " + token.getSurfaceForm());
+                        }
                     }
 
-                    if (System.Console.ReadKey().KeyChar == 'q')
+                    if (CliConsole.ReadKey().KeyChar == 'q')
                     {
                         break;
                     }
@@ -36,7 +50,7 @@ namespace TameIrofText
             catch (TwitterException te)
             {
                 te.printStackTrace();
-                System.Console.WriteLine("Failed to get timeline: " + te.getMessage());
+                CliConsole.WriteLine("Failed to get timeline: " + te.getMessage());
                 System.Environment.Exit(-1);
             }
         }
